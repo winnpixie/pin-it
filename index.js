059420cd269ff3bccab8fde8da25072133b9bb3c
Summer <salvadin2@gmail.com>
2020-07-11 22:49:11 -0400

    rewrite http capture again

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 (function () {
     'use strict';
 
-    // https://stackoverflow.com/questions/921198/get-request-url-from-xhr-object
-    const _open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function () {
-        this.resource = arguments[1];
-        _open.apply(this, arguments);
-    }
+    class AnyPin {
+        startNetCaptureService() {
+            window.netRequestCallbacks = [];
 
-    // My brain is bigger than the entirety of the multi-verse
-    const _srh = XMLHttpRequest.prototype.setRequestHeader;
-    XMLHttpRequest.prototype.setRequestHeader = function () {
-        if (this.headers == null) {
-            this.headers = [];
-        }
-        this.headers[arguments[0]] = arguments[1];
-        _srh.apply(this, arguments);
-    }
+            // Append a 'uri' value to each XMLHttpRequest
+            const _open = XMLHttpRequest.prototype.open;
+            XMLHttpRequest.prototype.open = function () {
+                this.uri = arguments[1];
+                _open.apply(this, arguments);
+            }
 
-    // https://stackoverflow.com/questions/34862749/alternative-to-ajaxcomplete-of-jquery-in-javascript-for-xmlhttprequest-or-act
-    let requestCallbacks = [];
-    const _send = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function () {
-        if (this.resource != null) {
-            let _orsc = this.onreadystatechange;
-            this.onreadystatechange = function () {
-                if (this.readyState === XMLHttpRequest.DONE) {
-                    requestCallbacks.forEach(cb => cb(this));
+            // Add the specified header to the map of headers for this XMLHttpRequest
+            const _srh = XMLHttpRequest.prototype.setRequestHeader;
+            XMLHttpRequest.prototype.setRequestHeader = function () {
+                if (this.headers == null) {
+                    this.headers = [];
                 }
-                _orsc.apply(this, arguments);
+                this.headers[arguments[0]] = arguments[1];
+                _srh.apply(this, arguments);
+            }
+
+            // Capture this XMLHttpRequest to call our listeners when it's completed.
+            const _send = XMLHttpRequest.prototype.send;
+            XMLHttpRequest.prototype.send = function () {
+                let _orsc = this.onreadystatechange;
+                this.onreadystatechange = function () {
+                    if (this.readyState === XMLHttpRequest.DONE) {
+                        netRequestCallbacks.forEach(func => func(this));
+                    }
+                    _orsc.apply(this, arguments);
+                }
+                _send.apply(this, arguments);
             }
         }
-        _send.apply(this, arguments);
-    }
-
-    class AnyPin {
         initialize() {
+            this.startNetCaptureService();
+
             // Register the callback to listen to /retweet.json
-            requestCallbacks.push(xhr => {
-                if (xhr.resource.endsWith('/retweet.json')) {
+            netRequestCallbacks.push(xhr => {
+                if (xhr.uri.endsWith('/retweet.json')) {
                     if (prompt('Would you like to pin this tweet to your profile? [y/N]', 'n') === 'y') {
                         let jsonObj = JSON.parse(xhr.responseText);
                         let token = xhr.headers['authorization'];
